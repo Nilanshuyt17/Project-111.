4bec825c55e5378c8e533303c53614183573e30e
Nilanshuyt17 <91885068+Nilanshuyt17@users.noreply.github.com>
2022-03-11 20:55:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var prediction_1 = "";
+var prediction_2 = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 100
+});
+
+camera = document.getElementById("Webcam1");
+
+Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_img' src = '" + data_uri + "'>";
+    });
+}
+
+console.log("ml5 version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/bN2OjBt2F/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
